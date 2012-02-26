85963bd030e027b6b69591ee5fcbda905ec4fe43
Oscar Javier Gentilezza Arenas <tioscar@gmail.com>
2012-02-26 13:03:28 -0300

    Terminado lo de los scopes y binds de funciones (me olvide de agregar files)

---
+++ lib/proto/scopevar.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Description of ScopeVar
+ *
+ * @author exos
+ */
+
+namespace Proto;
+
+class ScopeVar {
+    
+    public $value;
+    
+    public function __construct ($value) {
+        $this->value = $value;
+    }
+    
+}
---
+++ lib/proto/selfargument.php
@@ -0,0 +1,67 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of SelfArgument
+ *
+ * @author exos
+ */
+
+namespace Proto;
+
+use \Proto\Exceptions\Defintion as DefinitionException;
+
+class SelfArgument extends Scope {
+        
+    public function __construct(Scope $scope) {
+        $this->setScope($scope);
+    }
+    
+    function __get ($var) {
+        
+        if (isset($this->_scope->$var)) {
+            return $this->_scope->$var;
+        } elseif ($scope = $this->_scope->is_set($var)) {
+            return $scope->$var;
+        } else {
+            throw new DefinitionException ($var . " dont exist in this scope space");
+        }
+        
+    }
+    
+    function __set ($var, $value) {
+    
+        if (is_object($value) && is_a($value,'\Proto\ScopeVar')) {
+            parent::__set($var, $value->value);
+            unset($value);
+        } else {
+
+            if ($scope = $this->is_set($var)) {
+                $scope->$var = $value;
+            } else {
+                parent::__set($var,$value);
+            }
+
+        }
+    }
+    
+    function call ($func, $args, $bind = null) {
+        
+        $self = is_null($bind) ? $this : $bind;
+        
+        if (isset($this->_scope->$func)) {
+            if (is_callable($this->_scope->$func)) {
+                return $this->_scope->call($func, $args, $self);
+            }
+        } elseif ($scope = $this->_scope->is_set($func)) {
+            return $scope->call($func, $args, $self);
+        } else {
+            throw new DefinitionException ($func . " dont exist in this scope space");
+        }
+    }
+    
+}
